e61fcf751b123a0940c1df68d3e7a251d7b037bf
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-05-01 12:33:26 -0400

    some psuedocoding done

---
+++ server.js
@@ -0,0 +1,12 @@
+// =========================================================== Psuedo-Code =========================================================== //
+
+
+// 1) User clicks survey button ---> route to path with the survey
+    // a. Establish Paths (Home, Survey, Default)
+// 2) User fills out answers to questions
+    // a. Scroll down list options for everything, 1 - 5, must answer questions
+    // b. Input validation for name and photo
+        // -------> The data from user answers gets pushed to the peoples array on the apiRoute
+        // -------> The math gets done to find the total difference
+        // -------> The user gets a modal with the human who is their mate
+// 3) User is returned to home screen
