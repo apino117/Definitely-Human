24536ecd2b6d3310ff469653107d3c4d48e7a701
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-05-03 14:54:00 -0400

    added clear and got post working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,11 +9,24 @@ module.exports = function (app) {
 
     // Get Route
     app.get("/api/humans", function (req, res) {
+
         res.json(humansData);
+
     });
 
     // Post Route
-    // app.post("/api/humans", function(req, res) {
+    app.post("/api/humans", function (req, res) {
 
-    // });
+        humansData.push(req.body);
+        res.json();
+
+    });
+
+    // Clear Route
+    app.post("/api/clear", function(req, res) {
+        // Empty out the arrays of data
+        humansData.length = 0;
+    
+        res.json({ ok: true });
+      });
 };
