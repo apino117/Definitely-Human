bc5273ec17f8ba8f853de7644eaf0b569bc0687a
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-05-03 15:45:48 -0400

    total difference function not working where it is, scoping issue

--- app/data/humans.js
+++ app/data/humans.js
@@ -15,23 +15,4 @@ var humansArray = [
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = humansArray;
 
-// ======================================== Function to Do Math (Copy Pasta Out When Done) ======================================== //
 
-scoreArrayOne = humansArray[0].Scores;
-scoreArrayTwo = humansArray[1].Scores;
-
-function findTotalDifference(scoreArrayOne, scoreArrayTwo) {
-
-    var totalDifferenceArray = [];
-    for (i = 0; i < 10; i++) {
-        totalDifferenceArray.push(Math.abs(scoreArrayOne[i] - scoreArrayTwo[i]));
-    };
-
-    console.log(totalDifferenceArray.reduce(add));
-
-    function add(currentTotal, nextNumber) {
-        return currentTotal + nextNumber;
-    };
-};
-
-findTotalDifference(scoreArrayOne, scoreArrayTwo);
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,16 +17,52 @@ module.exports = function (app) {
     // Post Route
     app.post("/api/humans", function (req, res) {
 
+        // ======================================== Function to Do Math ======================================== //
+
+        // This one will have to be a loop through all the scores in the array, so it'll be the current score at a given point
+        // scoreArrayOne = humansArray[i].Scores;
+
+        // This one will be the newest addition to our array
+        // scoreArrayTwo = req.body.Scores;
+
+
+        function findTotalDifference(scoreArrayOne, scoreArrayTwo) {
+
+            // Establish blank array for purity
+            var totalDifferenceArray = [];
+
+            // Loop through both arrays at at each index get the absolute value of their difference
+            for (i = 0; i < 10; i++) {
+                totalDifferenceArray.push(Math.abs(scoreArrayOne[i] - scoreArrayTwo[i]));
+            };
+
+
+            // Not 100 % on why this works but it does
+            console.log(totalDifferenceArray.reduce(add));
+            function add(currentTotal, nextNumber) {
+                return currentTotal + nextNumber;
+            };
+        };
+
+        // Loop through each human in the array
+        for (i = 0; i < humansArray.length; i++) {
+
+            findTotalDifference(humansArray[i].scores, req.body.scores);
+
+        };
+
+        // ======================================== End of Math ======================================== //
+
         humansData.push(req.body);
-        res.json();
+        res.json(true);
 
     });
 
     // Clear Route
-    app.post("/api/clear", function(req, res) {
+    app.post("/api/clear", function (req, res) {
         // Empty out the arrays of data
         humansData.length = 0;
-    
+
         res.json({ ok: true });
-      });
+    });
 };
