e481167a7e640ebb36a27f0d14b9fef5935b6b03
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-05-07 16:29:36 -0400

    got some functionality in find difference

--- app/data/humans.js
+++ app/data/humans.js
@@ -1,14 +1,14 @@
 
 var humansArray = [
     {
-        Name: "Johnny Realman",
-        Photo: "https://vignette.wikia.nocookie.net/supersmashbrosfanon/images/2/24/Johnny_Bravo.jpg",
-        Scores: ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5",],
+        name: "Johnny Realman",
+        photo: "https://vignette.wikia.nocookie.net/supersmashbrosfanon/images/2/24/Johnny_Bravo.jpg",
+        scores: ["1", "2", "3", "4", "5", "1", "2", "2", "4", "5",],
     },
     {
-        Name: "Slurms Mckenzie",
-        Photo: "https://cdn.shopify.com/s/files/1/0045/3561/5599/products/C750351_1200x.jpg?v=1545967154",
-        Scores: ["5", "4", "3", "2", "1", "5", "4", "3", "2", "1"]
+        name: "Slurms Mckenzie",
+        photo: "https://cdn.shopify.com/s/files/1/0045/3561/5599/products/C750351_1200x.jpg?v=1545967154",
+        scores: ["5", "4", "3", "2", "1", "5", "4", "3", "2", "1"]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,43 +17,85 @@ module.exports = function (app) {
     // Post Route
     app.post("/api/humans", function (req, res) {
 
+        var globalSums = [];
+
+        // the req.body is the new human getting pushed
+
+        var newHuman = req.body;
+
+        humansData.push(newHuman);
+
+        // The humans data is the humans array
+
         // ======================================== Function to Do Math ======================================== //
 
         // This one will have to be a loop through all the scores in the array, so it'll be the current score at a given point
         // scoreArrayOne = humansArray[i].Scores;
 
         // This one will be the newest addition to our array
-        // scoreArrayTwo = req.body.Scores;
+        // scoreArrayTwo = newHuman.Scores;
 
 
         function findTotalDifference(scoreArrayOne, scoreArrayTwo) {
 
-            // Establish blank array for purity
-            var totalDifferenceArray = [];
+            // Establish blank array for purity - this array will hold things while they're moving
+            var temporaryArray = [];
 
             // Loop through both arrays at at each index get the absolute value of their difference
             for (i = 0; i < 10; i++) {
-                totalDifferenceArray.push(Math.abs(scoreArrayOne[i] - scoreArrayTwo[i]));
+                temporaryArray.push(Math.abs(scoreArrayOne[i] - scoreArrayTwo[i]));
             };
 
 
-            // Not 100 % on why this works but it does
-            console.log(totalDifferenceArray.reduce(add));
-            function add(currentTotal, nextNumber) {
-                return currentTotal + nextNumber;
+            // This is the for loop version of the reduce method, it doesnt use return which was exiting loops early later on in the functions
+            var sum = 0;
+
+            for (var i = 0; i < temporaryArray.length; i++) {
+                sum += temporaryArray[i];
             };
+
+            console.log(sum);
+
+            globalSums.push(parseInt(sum));
+
+            // // Not 100 % on why this works but it does - it adds everything in the array together
+
+            // console.log(temporaryArray.reduce(add));
+
+            // function add(currentTotal, nextNumber) {
+            //     return currentTotal + nextNumber;
+            // };
         };
+
+        // Test case with just one static array
+        // findTotalDifference(humansData[0].scores, newHuman.scores);
+
+
+        // Create Special Number that will be one less than the humans data array length - this is so when we loop we leave off the last addiition (the one being tested)
+
+        var specialNumber = humansData.length - 1;
+
+        // console.log("special number is " + specialNumber);
 
         // Loop through each human in the array
-        for (i = 0; i < humansArray.length; i++) {
+        for (i = 0; i < specialNumber; i++) {
 
-            findTotalDifference(humansArray[i].scores, req.body.scores);
+            findTotalDifference(humansData[i].scores, newHuman.scores);
 
         };
+        
+        // Get the sums in an array
+        console.log("Global sums " + globalSums);
+
+        // Find the lowest number in the array - thats the match because there are the fewest differences
+
+        // Return the JSON info that is in accordance with the match
+
+
+
 
         // ======================================== End of Math ======================================== //
 
-        humansData.push(req.body);
         res.json(true);
 
     });
@@ -66,3 +108,5 @@ module.exports = function (app) {
         res.json({ ok: true });
     });
 };
+
+
